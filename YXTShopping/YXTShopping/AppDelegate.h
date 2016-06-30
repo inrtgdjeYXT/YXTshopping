c83eb9130e820e85a7cc958e2afaea376c63cbaa
inrtgdje <ttm0402@163.com>
2016-06-30 21:07:06 +0800

    创建新的项目新建文件夹

---
+++ YXTShopping/YXTShopping/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YXTShopping
+//
+//  Created by 汤天明 on 16/6/30.
+//  Copyright © 2016年 easysay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
